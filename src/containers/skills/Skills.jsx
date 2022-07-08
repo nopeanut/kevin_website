e385f2af10b668d18dc8f895e5b3870b4cec8b97
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-07-08 13:44:06 -0400

    made the grid template for skills

--- src/containers/skills/Skills.jsx
+++ src/containers/skills/Skills.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./skills.css"
+import tempicon from '../../assets/tempicon.png';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='the_skills_main'>
+      <div className='the_skills_title'>
+        <h1>Skills</h1>
+      </div>
+      <div className='the_skills_list'>
+        <div className='the_skills_list1'>
+          <img src={tempicon} alt="Python"/>
+          <img src={tempicon} alt="Java"/>
+          <img src={tempicon} alt="C"/>
+          <img src={tempicon} alt="CSS"/>
+        </div>
+        <div className='the_skills_list2'>
+          <img src={tempicon} alt="JavaScript"/>
+          <img src={tempicon} alt="Git"/>
+          <img src={tempicon} alt="HTML"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
