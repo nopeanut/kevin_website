f501ef68fa98fd8f3e9bb389e6babeab35aedd27
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-07-14 19:50:03 -0400

    form changes

--- src/components/form/TheForm.jsx
+++ src/components/form/TheForm.jsx
@@ -9,12 +9,20 @@ const TheForm = () => {
             <form name = "contact-form" method = "POST" data-netlify = "true">
                 <input type = "hidden" name = "form-name" value = "contact-form" />
                 <div className='the_form_stuff'>
-                    <label for="name">Name</label>
-                    <input type = "text" placeholder = "enter your name" name = "name"/>
+                    <div className='the_form_input_name'>
+                        <label for="name">Name</label>
+                        <input type = "text" placeholder = "enter your name" name = "name"/>
+                    </div>
+                    <div className='the_form_input_email'>
+                        <label for="email">Email</label>
+                        <input type = "text" placeholder = "enter your email" name = "email"/>
+                    </div>
+                    <div className='the_form_input_message'>
+                        <label for="message">Message</label>
+                        <textarea placeholder = "enter your message" name = "message"/>
+                    </div>
+                    <button type = "submit">Submit</button>
                 </div>
-                <input type = "text" placeholder = "enter your email" name = "email"/>
-                <textarea placeholder = "enter your message" name = "message"/>
-                <button type = "submit">Submit</button>
             </form>
         </div>
     </div>
