34cc9ae43c0301db9c147a82c2f4c3c794169a49
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-07-14 19:31:10 -0400

    final form

--- src/components/form/TheForm.jsx
+++ src/components/form/TheForm.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import './theForm.css';
-import Form from 'react-bootstrap/Form';
 
 const TheForm = () => {
   return (
     <div className='the_form_main'>
-        <form
-        name = "contact-form"
-        method = "POST"
-        data-netlify = "true"
-        >
-            <input type = "hidden" name = "form-name" value = "contact-form" />
-            <input type = "text" placeholder = "enter your name" name = "name"/>
-            <input type = "text" placeholder = "enter your email" name = "email"/>
-            <textarea placeholder = "enter your message" name = "message"/>
-            <button type = "submit">Submit</button>
-        </form>
+        <h1>Get in touch</h1>
+        <div class="the_form_input">
+            <form name = "contact1" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact1"/>
+                <div className='the_form_stuff'>
+                    <div className='the_form_input_name'>
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder=""/>
+                    </div>
+                    <div className='the_form_input_email'>
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" placeholder="" />
+                    </div>
+                    <div className='the_form_input_message'>
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" placeholder=""/>
+                    </div>
+                    <div className='the_form_input_button'>
+                        <input type="submit" value="Submit" />
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
