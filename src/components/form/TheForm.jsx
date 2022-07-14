26ffb399ff526c7c4cde8425de2549a8c527643c
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-07-14 19:45:45 -0400

    testing form #2

--- src/components/form/TheForm.jsx
+++ src/components/form/TheForm.jsx
@@ -4,10 +4,14 @@ import './theForm.css';
 const TheForm = () => {
   return (
     <div className='the_form_main'>
-        <div className='the_form_main'>
+        <h1>Get in touch</h1>
+        <div className='the_form_input'>
             <form name = "contact-form" method = "POST" data-netlify = "true">
                 <input type = "hidden" name = "form-name" value = "contact-form" />
-                <input type = "text" placeholder = "enter your name" name = "name"/>
+                <div className='the_form_stuff'>
+                    <label for="name">Name</label>
+                    <input type = "text" placeholder = "enter your name" name = "name"/>
+                </div>
                 <input type = "text" placeholder = "enter your email" name = "email"/>
                 <textarea placeholder = "enter your message" name = "message"/>
                 <button type = "submit">Submit</button>
