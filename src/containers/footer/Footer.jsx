555797f34aabe2e92473a7841a3f8a8a7de5c19a
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-07-13 21:45:29 -0400

    Changes to style

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <div className='the_footer_main'>
         <div className='the_footer_icons'>
-            <a href='https://www.facebook.com/kevin.li.50746/'><FaFacebook /></a>
-            <a href='https://www.instagram.com/keviin.li/'><FaInstagram /></a>
-            <a href='https://www.linkedin.com/in/kevinli7022/'><FaLinkedin /></a>
-            <a href='https://github.com/nopeanut'><FaGithub/></a>
+            <a className='the_footer_icons_stuff' href='https://www.facebook.com/kevin.li.50746/'><FaFacebook /></a>
+            <a className='the_footer_icons_stuff' href='https://www.instagram.com/keviin.li/'><FaInstagram /></a>
+            <a className='the_footer_icons_stuff' href='https://www.linkedin.com/in/kevinli7022/'><FaLinkedin /></a>
+            <a className='the_footer_icons_stuff' href='https://github.com/nopeanut'><FaGithub/></a>
         </div>
         <p>Copyright 2022</p>
     </div>
