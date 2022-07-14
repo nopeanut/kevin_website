6c947649167aa33d4645bb470856cb31acf0b887
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-07-14 19:37:33 -0400

    working form

--- src/components/form/TheForm.jsx
+++ src/components/form/TheForm.jsx
@@ -4,27 +4,13 @@ import './theForm.css';
 const TheForm = () => {
   return (
     <div className='the_form_main'>
-        <h1>Get in touch</h1>
-        <div class="the_form_input">
-            <form name = "contact1" method="POST" data-netlify="true">
-                <input type="hidden" name="form-name" value="contact1"/>
-                <div className='the_form_stuff'>
-                    <div className='the_form_input_name'>
-                        <label for="name">Name</label>
-                        <input type="text" name="name" placeholder=""/>
-                    </div>
-                    <div className='the_form_input_email'>
-                        <label for="email">Email</label>
-                        <input type="text" name="email" placeholder="" />
-                    </div>
-                    <div className='the_form_input_message'>
-                        <label for="message">Message</label>
-                        <textarea name="message" placeholder=""/>
-                    </div>
-                    <div className='the_form_input_button'>
-                        <button type="submit">Submit</button>
-                    </div>
-                </div>
+        <div className='the_form_main'>
+            <form name = "contact-form" method = "POST" data-netlify = "true">
+                <input type = "hidden" name = "form-name" value = "contact-form" />
+                <input type = "text" placeholder = "enter your name" name = "name"/>
+                <input type = "text" placeholder = "enter your email" name = "email"/>
+                <textarea placeholder = "enter your message" name = "message"/>
+                <button type = "submit">Submit</button>
             </form>
         </div>
     </div>
