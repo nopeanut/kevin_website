45b9efdd5eb6fef7f916a4449e0e1d0eea709154
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-11-01 12:50:48 -0400

    Added a quote section using API

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Navbar} from './components';
 import {Header, Footer} from './containers';
 import './Home.css';
-
 function Home() {
   return (
     <div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './header.css';
 import profile from '../../assets/profile.jpg';
-
+import axios from "axios";
 const Header = () => {
+
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const quoteAPI = async () => {
+    let arrayOfQuotes = [];
+    try {
+      const data = await axios.get("https://api.quotable.io/random");
+      arrayOfQuotes = data.data
+    }
+    catch (error) {
+      console.log(error);
+    }
+    try {
+      setQuote(arrayOfQuotes.content);
+      setAuthor(arrayOfQuotes.author);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    quoteAPI();
+  }, [])
   return (
     <div className='the_main'>
         <div className='the_main_bio'>
@@ -14,6 +39,18 @@ const Header = () => {
           </div>
           <div className='the_main_p_2'>
             <p>I am currently a third-year computer engineering undergraduate attending Ryerson University. Currently focusing on self-improvement to enhance my qualities.</p>
+          </div>
+          {/* Quote API */}
+          <div className = 'the_main_magic_box'>
+            <div className = 'the_main_magic_box_title'>
+              <h1>Inspirational Quote</h1>
+            </div>
+            <div className='the_main_magic_box_quote'>
+              <p>"{quote}"</p>
+            </div>
+            <div className='the_main_magic_box_author'>
+              <p>- {author}</p>
+            </div>
           </div>
         </div>
         <div className='the_main_photo'>
