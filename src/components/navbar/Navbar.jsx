fe49ded3cdf9e723eb5e86cf922a03fb3e40efa1
nopeanut <65178913+nopeanut@users.noreply.github.com>
2022-11-03 19:50:51 -0400

    updated resume

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                 </div>
             </div>
             <div className='the__navbar-links-resume'>
-                <a href= 'https://drive.google.com/file/d/1ZGka8mthj5SNWlo5UaAlqdtupR0m_mQo/view?usp=sharing' target = "_black"><button type='button'>Resume</button></a>
+                <a href= 'https://drive.google.com/file/d/1Nat_mhcLNVthf0pUApqilM6xUGRKH0a_/view?usp=sharing' target = "_black"><button type='button'>Resume</button></a>
             </div>
         </div>
     </div>
